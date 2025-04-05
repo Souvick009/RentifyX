f92c3620768674b31ba03f19bd87aac5880ca99d
Rameshwar <129955992+rameshwar-mishra@users.noreply.github.com>
2025-04-05 16:38:21 +0530

    Created Welcome Screen, Persistent to one Theme across both light and dark theme

--- app/src/main/java/com/example/rentifyx/ui/theme/Type.kt
+++ app/src/main/java/com/example/rentifyx/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.example.rentifyx.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.rentifyx.R
 
-// Set of Material typography styles to start with
+val MontserratBold = FontFamily(
+    Font(R.font.montserrat_black, FontWeight.Bold)
+)
+val MontserratNormal = FontFamily(
+    Font(R.font.montserrat_light, FontWeight.Normal)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MontserratBold,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.7.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = TextStyle(
+        fontFamily = MontserratBold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 30.sp,
+        letterSpacing = 0.6.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = MontserratNormal,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.7.sp
     )
-    */
 )
