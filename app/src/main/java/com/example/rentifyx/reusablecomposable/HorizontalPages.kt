ccdf5f91aeaf4b31179d5dc8859b0e46cb3a76dd
Ram <rameshwarmishra2003@gmail.com>
2025-04-06 17:00:38 +0530

    Fixed Some layout issue in welcome screen, added horizontal pager(used for slider), LazyRow and LazyColumn in HomeScreen.

---
+++ app/src/main/java/com/example/rentifyx/reusablecomposable/HorizontalPages.kt
@@ -0,0 +1,35 @@
+package com.example.rentifyx.reusablecomposable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun HorizontalPages(
+    modifier: Modifier = Modifier,
+    titleText : String
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                MaterialTheme.colorScheme.primary
+            )
+    ) {
+        Text(
+            titleText,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onPrimary,
+            textAlign = TextAlign.Center
+        )
+    }
+}
