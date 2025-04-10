608fbee23fefbe070bfe45ba7a9a59665a2eafc9
Rameshwar <129955992+rameshwar-mishra@users.noreply.github.com>
2025-04-10 14:25:57 +0530

    Added Google Logo in Sign in Button, Made Changes in Cards of Home Screen.

--- app/src/main/java/com/example/rentifyx/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/rentifyx/screens/WelcomeScreen.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -21,9 +24,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -31,7 +31,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -175,12 +174,28 @@ fun WelcomeScreen(navController: NavController, authViewModel: AuthViewModel) {
                             end.linkTo(endGuideline)
                             width = Dimension.fillToConstraints
                         },
-                    shape = RoundedCornerShape(30.dp)
-
+                    shape = RoundedCornerShape(30.dp),
+                    elevation = ButtonDefaults.buttonElevation(
+                        defaultElevation = 5.dp,
+                        pressedElevation = 10.dp,
+                        focusedElevation = 5.dp,
+                        hoveredElevation = 7.dp,
+                        disabledElevation = 0.dp
+                    ),
                 ) {
                     if (authViewModel.isLoading.value) {
-                        CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+                        CircularProgressIndicator(
+                            color = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier.size(25.dp)
+                        )
                     } else {
+                        Image(
+                            painter = painterResource(R.drawable.google_icon),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(26.dp)
+                        )
+                        Spacer(Modifier.width(8.dp))
                         Text("Sign In with Google", style = MaterialTheme.typography.bodyLarge)
                     }
                 }
